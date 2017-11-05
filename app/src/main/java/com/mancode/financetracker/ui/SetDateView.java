ed0b5e91e79f1846c7ea9589de37a62ac09b34f3
Michal Dominiczak <michal-dominiczak@o2.pl>
2017-11-05 22:31:36 +0100

    Custom SetDateView created (Button), styling slightly corrected

---
+++ app/src/main/java/com/mancode/financetracker/ui/SetDateView.java
@@ -0,0 +1,63 @@
+package com.mancode.financetracker.ui;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.mancode.financetracker.R;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Manveru on 04.11.2017.
+ */
+
+public class SetDateView extends AppCompatButton {
+
+    private Calendar mCalendar;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    public SetDateView(Context context) {
+        this(context, null);
+    }
+
+    public SetDateView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mCalendar = Calendar.getInstance();
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                mCalendar.set(year, month, dayOfMonth);
+                updateText();
+            }
+        };
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(
+                        getContext(),
+                        R.style.AppTheme_DatePicker,
+                        mDateSetListener,
+                        mCalendar.get(Calendar.YEAR),
+                        mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+    }
+
+    public Date getDate() {
+        return mCalendar != null ? mCalendar.getTime() : null;
+    }
+
+    private void updateText() {
+        Date date = mCalendar.getTime();
+        DateFormat df = DateFormat.getDateInstance();
+        this.setText(df.format(date));
+    }
+}
