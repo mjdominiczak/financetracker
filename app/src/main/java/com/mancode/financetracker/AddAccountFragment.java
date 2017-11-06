42f4a5ba2481fbaa164dbd19de99066c603ec1d0
Michał Dominiczak <michal-dominiczak@o2.pl>
2017-11-06 23:03:47 +0100

    Open and close date added in AddAccountFragment

--- app/src/main/java/com/mancode/financetracker/AddAccountFragment.java
+++ app/src/main/java/com/mancode/financetracker/AddAccountFragment.java
@@ -11,6 +11,11 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.mancode.financetracker.database.DatabaseContract;
+import com.mancode.financetracker.ui.SetDateView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class AddAccountFragment extends AddItemFragment {
@@ -30,11 +35,17 @@ public class AddAccountFragment extends AddItemFragment {
                 if (item.getItemId() == R.id.action_menu_save) {
                     String name = ((EditText) getView().findViewById(R.id.tf_account_name)).getText().toString();
                     String type = ((EditText) getView().findViewById(R.id.tf_account_type)).getText().toString();
+                    Date date = ((SetDateView) getView().findViewById(R.id.sd_account_open_date)).getDate();
+                    String openDate = new SimpleDateFormat(DatabaseContract.DATE_FORMAT_STRING, Locale.US).format(date);
+                    date = ((SetDateView) getView().findViewById(R.id.sd_account_close_date)).getDate();
+                    String closeDate = new SimpleDateFormat(DatabaseContract.DATE_FORMAT_STRING, Locale.US).format(date);
 
                     if (AccountListItem.validate(name, type)) {
                         ContentValues cv = new ContentValues();
                         cv.put(DatabaseContract.AccountEntry.COL_NAME, name);
                         cv.put(DatabaseContract.AccountEntry.COL_TYPE, type);
+                        cv.put(DatabaseContract.AccountEntry.COL_OPEN_DATE, openDate);
+                        cv.put(DatabaseContract.AccountEntry.COL_CLOSE_DATE, closeDate);
                         getActivity().getContentResolver().insert(DatabaseContract.AccountEntry.CONTENT_URI, cv);
                         dismiss();
                     }
