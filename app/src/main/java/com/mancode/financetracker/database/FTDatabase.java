c9306f9f17351dffa7b24135347876bb34975f8b
Michal Dominiczak <michal-dominiczak@o2.pl>
2018-12-22 22:15:06 +0100

    Database version reset to 1

--- app/src/main/java/com/mancode/financetracker/database/FTDatabase.java
+++ app/src/main/java/com/mancode/financetracker/database/FTDatabase.java
@@ -20,7 +20,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
@@ -38,69 +37,9 @@ import androidx.work.WorkManager;
             version = FTDatabase.DATABASE_VERSION)
 public abstract class FTDatabase extends RoomDatabase {
 
-    public static final int DATABASE_VERSION = 6;
+    public static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "database.db";
     private static FTDatabase sInstance;
-    /**
-     * Migration from:
-     * version 1 - using SQLiteDatabase API
-     * to
-     * version 2 - using Room
-     */
-    private static Migration MIGRATION_1_2 = new Migration(1, 2) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-
-        }
-    };
-    /**
-     * Migration from:
-     * version 2
-     * to
-     * version 3 - using autoGenerate = true
-     */
-    private static Migration MIGRATION_2_3 = new Migration(2, 3) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-
-        }
-    };
-    /**
-     * Migration from:
-     * version 3
-     * to
-     * version 4 - view added
-     */
-    private static Migration MIGRATION_3_4 = new Migration(3, 4) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-
-        }
-    };
-    /**
-     * Migration from:
-     * version 4
-     * to
-     * version 5 - currency in accounts table referenced by symbol
-     */
-    private static Migration MIGRATION_4_5 = new Migration(4, 5) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-
-        }
-    };
-    /**
-     * Migration from:
-     * version 5
-     * to
-     * version 6 - category type column added
-     */
-    private static Migration MIGRATION_5_6 = new Migration(5, 6) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-
-        }
-    };
 
     private final MutableLiveData<Boolean> mIsDatabaseCreated = new MutableLiveData<>();
 
@@ -126,11 +65,6 @@ public abstract class FTDatabase extends RoomDatabase {
                         WorkManager.getInstance().enqueue(request);
                     }
                 })
-                .addMigrations(MIGRATION_1_2)
-                .addMigrations(MIGRATION_2_3)
-                .addMigrations(MIGRATION_3_4)
-                .addMigrations(MIGRATION_4_5)
-                .addMigrations(MIGRATION_5_6)
                 .build();
     }
 
