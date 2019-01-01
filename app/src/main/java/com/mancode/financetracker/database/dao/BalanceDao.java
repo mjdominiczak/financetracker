9a6955757debfef41a64359e178f5fe785ad11e4
Michal Dominiczak <michal-dominiczak@o2.pl>
2019-01-01 23:21:39 +0100

    Query columns specified for join queries
    There was a bug with multiple "_id" columns returned by a query which
    caused that transaction deletion was not possible (or inaccurate) -
    transaction id was the same as category id

--- app/src/main/java/com/mancode/financetracker/database/dao/BalanceDao.java
+++ app/src/main/java/com/mancode/financetracker/database/dao/BalanceDao.java
@@ -1,15 +1,15 @@
 package com.mancode.financetracker.database.dao;
 
+import com.mancode.financetracker.database.entity.BalanceEntity;
+import com.mancode.financetracker.database.entity.BalanceExtended;
+
+import java.util.List;
+
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
-
-import com.mancode.financetracker.database.entity.BalanceEntity;
-import com.mancode.financetracker.database.entity.BalanceExtended;
-
-import java.util.List;
 
 /**
  * Created by Manveru on 25.01.2018.
@@ -24,13 +24,11 @@ public interface BalanceDao {
     @Query("SELECT * FROM balances")
     List<BalanceEntity> getAllBalances();
 
-    // TODO queried columns to be optimized
-    @Query("SELECT * FROM balances INNER JOIN accounts ON balance_account_id = accounts._id " +
+    @Query("SELECT balances._id, balance_check_date, balance_value, account_name, account_type, " +
+            "account_currency " +
+            "FROM balances INNER JOIN accounts ON balance_account_id = accounts._id " +
             "ORDER BY date(balance_check_date) DESC, balance_account_id ASC")
     LiveData<List<BalanceExtended>> getBalancesForDisplay();
-
-    @Query("SELECT * FROM balances WHERE _id = :id")
-    BalanceEntity getBalanceById(int id);
 
     @Insert
     void insertBalance(BalanceEntity balance);
--- app/src/main/java/com/mancode/financetracker/database/dao/TransactionDao.java
+++ app/src/main/java/com/mancode/financetracker/database/dao/TransactionDao.java
@@ -19,9 +19,13 @@ import androidx.room.Query;
 @Dao
 public interface TransactionDao {
 
-    @Query("SELECT * FROM transactions LEFT JOIN accounts ON transaction_account = accounts._id " +
+    @Query("SELECT transactions._id, transaction_date, transaction_type, transaction_description, " +
+            "transaction_value, transaction_account, transaction_category, account_name, " +
+            "account_type, account_currency, account_open_date, account_close_date, " +
+            "category, category_type " +
+            "FROM transactions LEFT JOIN accounts ON transaction_account = accounts._id " +
             "LEFT JOIN categories ON transaction_category = categories._id " +
-            "ORDER BY date(transaction_date) DESC, _id DESC")
+            "ORDER BY date(transaction_date) DESC, transactions._id DESC")
     LiveData<List<TransactionFull>> getAllTransactionsLive();
 
     @Query("SELECT * FROM transactions")
