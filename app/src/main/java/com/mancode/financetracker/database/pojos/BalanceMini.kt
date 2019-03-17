38513304d67dcae0a825e8342a2584820acf375c
Michal Dominiczak <michal-dominiczak@o2.pl>
2019-03-17 22:17:13 +0100

    BalanceMini refactor

--- app/src/main/java/com/mancode/financetracker/database/pojos/BalanceMini.kt
+++ app/src/main/java/com/mancode/financetracker/database/pojos/BalanceMini.kt
@@ -5,18 +5,10 @@ import androidx.room.TypeConverters
 import com.mancode.financetracker.database.converter.DateConverter
 import org.threeten.bp.LocalDate
 
-class BalanceMini {
-
-    @ColumnInfo(name = "balance_check_date")
-    @TypeConverters(DateConverter::class)
-    var checkDate: LocalDate? = null
-
-    @ColumnInfo(name = "balance_value")
-    var value: Double = 0.toDouble()
-
-    @ColumnInfo(name = "account_type")
-    var accountType: Int = 0
-
-    @ColumnInfo(name = "account_currency")
-    var AccountCurrency: String? = null
-}
+class BalanceMini(
+        @ColumnInfo(name = "balance_check_date")
+        @TypeConverters(DateConverter::class) val checkDate: LocalDate,
+        @ColumnInfo(name = "balance_value") val value: Double,
+        @ColumnInfo(name = "account_type") val accountType: Int,
+        @ColumnInfo(name = "account_currency") val accountCurrency: String
+)
