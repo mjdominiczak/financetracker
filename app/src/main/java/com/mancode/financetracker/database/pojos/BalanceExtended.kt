7b781b9355938618e85931084a3011ee9cc135a0
Michal Dominiczak <michal-dominiczak@o2.pl>
2019-03-17 22:32:08 +0100

    BalanceExtended refactor

--- app/src/main/java/com/mancode/financetracker/database/pojos/BalanceExtended.kt
+++ app/src/main/java/com/mancode/financetracker/database/pojos/BalanceExtended.kt
@@ -5,10 +5,10 @@ import androidx.room.TypeConverters
 import com.mancode.financetracker.database.converter.DateConverter
 import org.threeten.bp.LocalDate
 
+@TypeConverters(DateConverter::class)
 class BalanceExtended(
         @ColumnInfo(name = "_id") val id: Int,
-        @ColumnInfo(name = "balance_check_date")
-        @TypeConverters(DateConverter::class) val checkDate: LocalDate,
+        @ColumnInfo(name = "balance_check_date") val checkDate: LocalDate,
         @ColumnInfo(name = "balance_value") val value: Double,
         @ColumnInfo(name = "account_name") val accountName: String,
         @ColumnInfo(name = "account_type") val accountType: Int,
