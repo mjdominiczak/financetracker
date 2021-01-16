f44aa89a506474e31eb57e0394c8214f46e56f36
Michal Dominiczak <michal-dominiczak@o2.pl>
2021-01-16 23:10:32 +0100

    CrashReportingTree planted

--- app/src/main/java/com/mancode/financetracker/MyApplication.kt
+++ app/src/main/java/com/mancode/financetracker/MyApplication.kt
@@ -1,6 +1,8 @@
 package com.mancode.financetracker
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.mancode.financetracker.ui.prefs.PreferenceAccessor
 import timber.log.Timber
@@ -11,6 +13,21 @@ class MyApplication : Application() {
         super.onCreate()
         AndroidThreeTen.init(this)
         PreferenceAccessor.init(this)
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    private inner class CrashReportingTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.ERROR || priority == Log.DEBUG) {
+                FirebaseCrashlytics.getInstance().log(message)
+                if (t != null) {
+                    FirebaseCrashlytics.getInstance().recordException(t)
+                }
+            }
+        }
     }
 }
