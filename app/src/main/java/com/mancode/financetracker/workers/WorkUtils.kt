fc5465e0ea8b765c49f501d27d1460151227c49a
Michal Dominiczak <michal-dominiczak@o2.pl>
2021-05-04 22:05:38 +0200

    Calling UpdateStateWorker corrected to enqueue partial work after complete update

--- app/src/main/java/com/mancode/financetracker/workers/WorkUtils.kt
+++ app/src/main/java/com/mancode/financetracker/workers/WorkUtils.kt
@@ -3,6 +3,7 @@ package com.mancode.financetracker.workers
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.mancode.financetracker.database.converter.DateConverter
@@ -28,43 +29,48 @@ fun Context.runUpdateWorker(accountIds: IntArray? = null, date: LocalDate? = nul
         addTag(TAG_UPDATE)
         if (isPartial) addTag(TAG_UPDATE_PARTIAL)
         setInputData(
-                Data.Builder().apply {
-                    if (accountIds != null) putIntArray(UpdateStateWorker.KEY_ACCOUNT_IDS, accountIds)
-                    if (date != null) putString(UpdateStateWorker.KEY_DATE, DateConverter.toString(date))
-                }.build())
+            Data.Builder().apply {
+                if (accountIds != null) putIntArray(UpdateStateWorker.KEY_ACCOUNT_IDS, accountIds)
+                if (date != null) putString(
+                    UpdateStateWorker.KEY_DATE,
+                    DateConverter.toString(date)
+                )
+            }.build()
+        )
     }.build()
-    WorkManager.getInstance(this).apply {
-        if (!isPartial) cancelAllWorkByTag(TAG_UPDATE)
-        enqueue(request)
-    }
+    val policy = if (!isPartial) ExistingWorkPolicy.REPLACE else ExistingWorkPolicy.APPEND
+    WorkManager.getInstance(this)
+        .enqueueUniqueWork("UpdateStateWorker", policy, request)
 }
 
 fun Context.runExportToUri(uri: Uri) {
     val request = OneTimeWorkRequest.Builder(ExportToJsonWorker::class.java)
-            .addTag(TAG_EXPORT)
-            .setInputData(
-                    Data.Builder().apply {
-                        putString(ExportToJsonWorker.KEY_URI_ARG, uri.toString())
-                    }.build())
-            .build()
+        .addTag(TAG_EXPORT)
+        .setInputData(
+            Data.Builder().apply {
+                putString(ExportToJsonWorker.KEY_URI_ARG, uri.toString())
+            }.build()
+        )
+        .build()
     WorkManager.getInstance(this).enqueue(request)
 }
 
 fun Context.runImportFromUri(uri: Uri) {
     val importRequest = OneTimeWorkRequest.Builder(ImportFromJsonWorker::class.java)
-            .addTag(TAG_IMPORT)
-            .setInputData(
-                    Data.Builder().apply {
-                        putString(ImportFromJsonWorker.KEY_URI_ARG, uri.toString())
-                    }.build())
-            .build()
+        .addTag(TAG_IMPORT)
+        .setInputData(
+            Data.Builder().apply {
+                putString(ImportFromJsonWorker.KEY_URI_ARG, uri.toString())
+            }.build()
+        )
+        .build()
     val updateRequest = OneTimeWorkRequest.Builder(UpdateStateWorker::class.java)
-            .addTag(TAG_UPDATE)
-            .build()
+        .addTag(TAG_UPDATE)
+        .build()
     WorkManager.getInstance(this).apply {
         cancelAllWorkByTag(TAG_UPDATE)
         beginWith(importRequest)
-                .then(updateRequest)
-                .enqueue()
+            .then(updateRequest)
+            .enqueue()
     }
 }
